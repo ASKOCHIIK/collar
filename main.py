ce27c3ed61c3830b42f10fe1e6b78406c6fd48b3
askatkulmanov <askatkulmanov1@gmail.com>
2025-04-02 17:51:22 +0600

    Collab for students

---
+++ main.py
@@ -0,0 +1,7 @@
+class Mentor:
+    def __init__(self, name: str, last_name: str):
+        self.name = name
+        self.last_name = last_name
+
+    def info(self):
+        return f'Hi my name is {self.name}\nMy last name {self.last_name}'
